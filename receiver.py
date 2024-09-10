fc41e09403975859fa3dc31780803c15ec876337
Sid Mitra <sidmitra.del@gmail.com>
2024-09-10 13:52:01 -0300

    Upgrade to python 3.12

--- receiver.py
+++ receiver.py
@@ -1,18 +1,20 @@
 from time import sleep
-from typing import Union
+from typing import TYPE_CHECKING
 
 import daiquiri
 from celery import Celery
 from celery.events.state import Task, Worker
 
-from exporters import Exporter
 from utils import is_event_type_task
+
+if TYPE_CHECKING:
+    from exporters import Exporter
 
 logger = daiquiri.getLogger(__name__)
 
 
 class CeleryEventReceiver:
-    def __init__(self, broker: str):
+    def __init__(self, broker: str) -> None:
         self.broker = broker
         self.celery_app = Celery(
             broker=self.broker,
@@ -20,14 +22,14 @@ class CeleryEventReceiver:
         self.exporters: list[Exporter] = []
         self.state = self.celery_app.events.State()
 
-    def attach(self, exporter):
+    def attach(self, exporter) -> None:
         self.exporters.append(exporter)
 
-    def _notify_exporters(self, event: Union[Task, Worker]):
+    def _notify_exporters(self, event: Task | Worker) -> None:
         for exporter in self.exporters:
             exporter.process_event(event)
 
-    def notify(self, event):
+    def notify(self, event) -> None:
         event_type: str = event.get("type")
         if not is_event_type_task(event_type):
             # Since we are only exporting metrics related to celery task
@@ -37,7 +39,7 @@ class CeleryEventReceiver:
         event_details, event_type = self.state.event(event)
         event, is_create_event = event_details
         logger.debug(
-            f"Receiver event: {event.uuid} {event.name}, {event.state} {event.timestamp} with data: {event}"
+            f"Receiver event: {event.uuid} {event.name}, {event.state} {event.timestamp} with data: {event}",
         )
         self._notify_exporters(event)
 
@@ -46,7 +48,8 @@ class CeleryEventReceiver:
             while True:
                 try:
                     receiver = self.celery_app.events.Receiver(
-                        connection, handlers={"*": self.notify}
+                        connection,
+                        handlers={"*": self.notify},
                     )
                     receiver.capture(limit=None, timeout=120)
                 except Exception:
--- store.py
+++ store.py
@@ -43,7 +43,7 @@ class TaskStore(ABC):
 
 
 class InMemoryStore(TaskStore):
-    def __init__(self, max_size):
+    def __init__(self, max_size) -> None:
         self.event_store = LRUCache(max_size)
         self.processing_queue = Queue()
 
@@ -56,15 +56,15 @@ class InMemoryStore(TaskStore):
     def get_events(self, task_id):
         return self.event_store.get(task_id)
 
-    def pop_task(self, task_id):
+    def pop_task(self, task_id) -> None:
         self.event_store.data.pop(task_id)
 
-    def add_event(self, task_id, state, event):
+    def add_event(self, task_id, state, event) -> None:
         event_dict = self.serialize(event)
         try:
             self.event_store[task_id][state] = event_dict
         except KeyError:
             self.event_store[event.uuid] = {event.state: event_dict}
 
-    def add_processable_task(self, task_id):
+    def add_processable_task(self, task_id) -> None:
         self.processing_queue.put(task_id)
